aadeb2f87fe002009548025480c94365bc1635e9
daniapm <daniajibellis@hotmail.com>
2021-12-23 21:02:45 -0500

    adding header

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,9 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>		/* Used for printing the binary trees */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,16 +13,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-}; binary_tree_t;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
 
+/* 0-binary_tree_node.c */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
