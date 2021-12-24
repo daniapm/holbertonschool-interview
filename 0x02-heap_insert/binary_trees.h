4afb8eba425ae91e86f72b775015c485e68e0bca
daniapm <daniajibellis@hotmail.com>
2021-12-23 21:10:50 -0500

    fix code

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdlib.h>
-#include <stddef.h>		/* Used for printing the binary trees */
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,7 +23,6 @@ typedef struct binary_tree_s
 
 typedef struct binary_tree_s heap_t;
 
-/* 0-binary_tree_node.c */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 
