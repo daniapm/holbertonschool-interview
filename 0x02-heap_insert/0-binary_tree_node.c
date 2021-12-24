ccd8a3c6a921f4c94e6d875528d789c3a0843e6d
daniapm <daniajibellis@hotmail.com>
2021-12-23 21:08:50 -0500

    fix code

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,23 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - binary tree node
+ * binary_tree_node -  creates a binary tree node
  * @parent: parent node of the node to create
  * @value: put in the new node
  *
- * Return: function that creates a binary tree node
+ * Return: new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-
 	if (new_node == NULL)
-    {
-        return (NULL);
-    }
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = NULL;
@@ -25,9 +21,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent)
-    {
-        new_node->parent = parent;
-    }
+		new_node->parent = parent;
 
 	return (new_node);
 }
