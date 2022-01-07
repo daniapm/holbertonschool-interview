88f97cae6729464bd00b5dc05b41bac66afc77a7
daniapm <daniajibellis@hotmail.com>
2022-01-06 22:54:02 -0500

    minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+    Minimum Operations
+    """
+    if (n < 2):
+        return 0
+    my_list = []
+    index = 1
+    while (n != 1):
+        index = index + 1
+        if n % index == 0:
+            while n % index == 0:
+                n /= i
+                my_list.append(index)
+            result = sum(my_list)
+    return result
