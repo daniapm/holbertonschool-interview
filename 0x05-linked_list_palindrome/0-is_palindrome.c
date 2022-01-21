524689beae73212cda90e18de20398420330c7b3
daniapm <daniajibellis@hotmail.com>
2022-01-21 03:09:34 -0600

    fix code

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,36 @@
 #include "lists.h"
 
 /**
+ * isPalindromeUtil - Recursively moves the pointers to check if palindrome
+ * @left: Will move from the start of the list
+ * @right: Will move from the end of the list
+ *
+ * Return: Either 0 or 1 depending if is a palindrome
+ */
+int isPalindromeUtil(listint_t **left, listint_t *right)
+{
+
+	if (right == NULL)
+		return (1);
+	
+	int isTrue;
+
+	isTrue = isPalindromeUtil(left, right->next);
+
+	if (isTrue == 0)
+		return (0);
+
+	if ((*left)->n == right->n)
+		isTrue = 1;
+	else
+		isTrue = 0;
+
+	*left = (*left)->next;
+
+	return (isTrue);
+}
+
+/**
  * is_palindrome - Function in C that checks if a singly linked list is a palindrome.
  * @head: head of the linked list
  *
@@ -15,34 +45,4 @@ int is_palindrome(listint_t **head)
 		return (1);
 
 	return (isPalindromeUtil(left, right));
-}
-
-/**
- * isPalindromeUtil - Recursively moves the pointers to check if palindrome
- * @left: Will move from the start of the list
- * @right: Will move from the end of the list
- *
- * Return: Either 0 or 1 depending if is a palindrome
- */
-int isPalindromeUtil(listint_t **left, listint_t *right)
-{
-
-	if (right == NULL)
-        return (1);
-
-    int isTrue;
-
-	isTrue = isPalindromeUtil(left, right->next);
-
-	if (isTrue == 0)
-        return (0);
-
-	if ((*left)->n == end->n)
-        isTrue = 1;
-	else
-        isTrue = 0;
-
-	*right = (*left)->next;
-
-	return (isTrue);
 }
