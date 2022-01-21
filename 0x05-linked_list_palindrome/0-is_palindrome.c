2545082730bebb16f023b0cd44bafee5d65e0efa
daniapm <daniajibellis@hotmail.com>
2022-01-21 02:26:52 -0600

    Linked list palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Function in C that checks if a singly linked list is a palindrome.
+ * @head: head of the linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t **left = head;
+	listint_t *right = *head;
+
+	if (*head == NULL)
+		return (1);
+
+	return (isPalindromeUtil(left, right));
+}
+
+/**
+ * isPalindromeUtil - Recursively moves the pointers to check if palindrome
+ * @left: Will move from the start of the list
+ * @right: Will move from the end of the list
+ *
+ * Return: Either 0 or 1 depending if is a palindrome
+ */
+int isPalindromeUtil(listint_t **left, listint_t *right)
+{
+
+	if (right == NULL)
+    {
+        return (1);
+    }
+
+    int isTrue;
+	isTrue = isPalindromeUtil(left, right->next);
+
+	if (isTrue == 0)
+    {
+		return (0);
+    }
+
+	if ((*left)->n == end->n)
+    {
+        isTrue = 1;
+    }
+	else
+    {
+        isTrue = 0;
+    }
+
+	*right = (*left)->next;
+
+	return (isTrue);
+}
