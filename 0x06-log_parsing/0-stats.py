23c8a5b0891c26f690563a6d54df404b51e8af33
daniapm <daniajibellis@hotmail.com>
2022-01-31 20:11:06 -0600

    script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,51 +5,43 @@ Write a script that reads stdin line by line and computes metrics
 from sys import stdin
 
 
-def print_status(file_size, status_codes):
+def printstats(file_size, status_codes):
     """
     prints statistics
     """
-    print("File size: {:d}".format(file_size))
-    for statusCode, times in sorted(status_codes.items()):
-        if times:
-            print("{:s}: {:d}".format(statusCode, times))
+    print("File size: " + str(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(code + ": " + str(status_codes[code]))
 
-status_codes = {"200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
-                }
 
-number = 0
-size = 0
-status = 0
-
+line_num = 0
+file_size = 0
+status_code = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
     for line in stdin:
-        number += 1
+        line_num += 1
         split_line = line.split()
 
         if len(split_line) > 1:
-            size += int(split_line[-1])
+            file_size += int(split_line[-1])
 
         if len(split_line) > 2 and split_line[-2].isnumeric():
-            status = split_line[-2]
+            status_code = split_line[-2]
         else:
-            status = 0
+            status_code = 0
 
-        if status in status_codes.keys():
-            status_codes[status] += 1
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
 
-        if number % 10 == 0:
-            print_status(size, status_codes)
+        if line_num % 10 == 0:
+            printstats(file_size, status_codes)
 
-    print_status(size, status_codes)
+    printstats(file_size, status_codes)
 
 except (KeyboardInterrupt):
-    print_status(size, status_codes)
+    printstats(file_size, status_codes)
     raise
