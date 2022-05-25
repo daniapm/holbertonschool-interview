95179a448e642ca9e8756943d2087dcd41f73dcf
daniapm <daniajibellis@hotmail.com>
2022-05-25 01:12:28 -0600

    fix header file

--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,17 +11,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
+#include <stdlib.h>
+
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
+int heap_extract(heap_t **root);
 
-void binary_tree_print(const binary_tree_t *tree);
-int extract(heap_t **root);
-
-#endif /* BINARY_TREES */
+#endif /* _BINARY_TREES_H_ */
