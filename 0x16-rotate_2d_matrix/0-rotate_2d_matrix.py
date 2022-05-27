ce6d9da2eac162294dc361f4a9ddad62d9f020d6
daniapm <daniajibellis@hotmail.com>
2022-05-27 00:59:30 -0600

    rotate matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Rotate it 90 degrees clockwise"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate 2d matrix
+    """
+
+    len_matrix = len(matrix[0])
+
+    for i in range(n - 1, -1, -1):
+        for j in range(0, len_matrix):
+            matrix[j].append(matrix[i].pop(0))
